ed4da691c0d012a8774d4361395af2befe4d2b46
George Stavroulakis <g.stavroulakis@gmail.com>
2022-02-25 09:41:13 +0100

    setting up

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    protected $namespace = 'App\\Http\\Controllers'; //cbp
+
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
